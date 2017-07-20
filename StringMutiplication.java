c060a06ee9d162f7ac10958d2e505fa408738e76
vedhanayaki <vedhanayaki1996@gmail.com>
2017-07-19 20:54:30 -0700

    Add files via upload

---
+++ StringMutiplication.java
@@ -0,0 +1,12 @@
+package player;
+
+public class StringMutiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String a="1234";
+String b="2";
+System.out.println(a+"*"+b);
+	}
+
+}
